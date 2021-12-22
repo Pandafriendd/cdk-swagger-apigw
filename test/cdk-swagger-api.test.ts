9f00126303640a3fbc8714f9d8a2a1569fc8536c
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-12-22 21:14:14 +0000

    Initial commit

---
+++ test/cdk-swagger-api.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkSwaggerApi from '../lib/cdk-swagger-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-swagger-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSwaggerApi.CdkSwaggerApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
